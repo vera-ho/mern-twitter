d25a525d4ba62245b8be01901b772f7650e7cf4e
Vera Ho <vh71886@gmail.com>
2022-07-01 17:08:01 -0400

    Add express routes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const mongoose = require('mongoose');
+import express from "express";
+import mongoose from "mongoose";
+import { db } from "./config/keys.js";
+import { router as users} from "./routes/api/users.js";
+import { router as tweets } from "./routes/api/tweets.js"
 
 const app = express();
-const db = require('./config/keys').mongoURI;
 
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db.mongoURI, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
@@ -14,3 +16,5 @@ app.get("/", (req, res) => res.send("Adopt a Dino"));
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
---
+++ routes/api/tweets.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+export const router = express.Router();
+router.get("/test", (req, res) => res.json({
+    msg: "This is the tweet's route"
+}));

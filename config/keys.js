1edcc0b1aa601ef54a74c3b1cad390145b0402f9
Vera Ho <vh71886@gmail.com>
2022-07-03 18:48:17 -0400

    print env from keys.js for debug

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,7 @@ import { db } from "./keys_dev.js"
 import { keys } from "./keys_prod.js"
 
 export let key;
+console.log(process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === 'production') {
     key = keys;

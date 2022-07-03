f1bb85d830cdf55900a9f6974b22c4accc81ee9e
Vera Ho <vh71886@gmail.com>
2022-07-03 17:33:45 -0400

    Prep for heroku deploy

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
@@ -13,6 +14,14 @@ import cors from "cors";
 // import expressListRoutes from "express-list-routes";  
 
 const app = express();
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
 app.use(cors());
 
 mongoose
